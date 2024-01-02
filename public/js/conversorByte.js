250d3a6fcdae78b1b935f3581193487fe46e903b
Joao in Perola <kandrocmatunein@gmail.com>
2024-01-02 14:45:41 -0300

    conversor de bytes

---
+++ public/js/conversorByte.js
@@ -0,0 +1,39 @@
+function convToMega(){
+  var entrada1 = document.getElementById('text1').value;
+  const var1= Number(entrada1);
+  const var2 = var1 * 1024;
+  document.getElementById("text2").value = var2;
+}
+
+function convToGiga(){
+  var entrada1 = document.getElementById('text3').value;
+  const var1= Number(entrada1);
+  const var2 = var1 / 1024;
+  document.getElementById("text4").value = var2;
+}
+
+function convGToKilo(){
+  var entrada1 = document.getElementById('text5').value;
+  const var1= Number(entrada1);
+  const var2 = var1 * 1024 * 1024;
+  document.getElementById("text6").value = var2;
+}
+
+function convKToGiga(){
+  var entrada1 = document.getElementById('text7').value;
+  const var1= Number(entrada1);
+  const var2 = var1 / 1024 / 1024;
+  document.getElementById("text8").value = var2;
+}
+
+// funcao para limpeza
+function limpar() {
+  // Obter todos os elementos de entrada
+  var camposInput = document.querySelectorAll('input');
+  // limpa todos os campos com 'name' ou 'id' começado em 'text'
+  camposInput.forEach(function (campo) {
+    if (campo.name.startsWith('text') || campo.id.startsWith('text')) {
+      campo.value = ''; // Limpar o valor do campo
+    }
+  });
+}
