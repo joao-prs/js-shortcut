b2034264d65b4e8b378e9f7562183277d0ec25e6
Joao in Perola <kandrocmatunein@gmail.com>
2024-01-11 07:23:45 -0300

    i need to fix these problems

---
+++ public/js/json.js
@@ -0,0 +1,12 @@
+// import fs module
+//const fs = require("fs");
+
+
+function registrar(){
+  var entrada1 = document.getElementById('text1').value;
+  var entrada2 = document.getElementById('text2').value;
+  var entrada3 = document.getElementById('text3').value;
+
+  let var1
+  document.getElementById("text2").value = var2;
+}
